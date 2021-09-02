d4df21024bf5d395c839044ae096d9a95995c365
Jyri Lindroos <jyri.lindroos@keuda.fi>
2021-09-02 09:15:18 +0300

    SuosikkiNimi valmiina

---
+++ SuosikkiNimi/SuosikkiNimi/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SuosikkiNimi
+{
+    partial class SuosittuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NimiLB = new System.Windows.Forms.Label();
+            this.NimiTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.TarkastaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NimiLB
+            // 
+            this.NimiLB.AutoSize = true;
+            this.NimiLB.Location = new System.Drawing.Point(13, 13);
+            this.NimiLB.Name = "NimiLB";
+            this.NimiLB.Size = new System.Drawing.Size(545, 24);
+            this.NimiLB.TabIndex = 0;
+            this.NimiLB.Text = "Anna nimesi, niin tarkastan, onko se 50 suosituimman joukossa: ";
+            // 
+            // NimiTB
+            // 
+            this.NimiTB.Location = new System.Drawing.Point(564, 13);
+            this.NimiTB.Name = "NimiTB";
+            this.NimiTB.Size = new System.Drawing.Size(189, 29);
+            this.NimiTB.TabIndex = 1;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(17, 58);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 24);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            // 
+            // TarkastaBT
+            // 
+            this.TarkastaBT.Location = new System.Drawing.Point(564, 58);
+            this.TarkastaBT.Name = "TarkastaBT";
+            this.TarkastaBT.Size = new System.Drawing.Size(189, 37);
+            this.TarkastaBT.TabIndex = 3;
+            this.TarkastaBT.Text = "Tarkasta";
+            this.TarkastaBT.UseVisualStyleBackColor = true;
+            this.TarkastaBT.Click += new System.EventHandler(this.TarkastaBT_Click);
+            // 
+            // SuosittuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 111);
+            this.Controls.Add(this.TarkastaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.NimiTB);
+            this.Controls.Add(this.NimiLB);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "SuosittuForm";
+            this.Text = "Onko nimesi suosittujen joukossa?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NimiLB;
+        private System.Windows.Forms.TextBox NimiTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button TarkastaBT;
+    }
+}
+
---
+++ SuosikkiNimi/SuosikkiNimi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SuosikkiNimi
+{
+    public partial class SuosittuForm : Form
+    {
+        public SuosittuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TarkastaBT_Click(object sender, EventArgs e)
+        {
+            VastausLB.Text = "";
+            VastausLB.Visible = false;
+            int laskurip = 1, laskurit = 1;
+            string[] pojat = File.ReadAllLines("C:\\Users\\jyrili\\source\\T1-tasonohjelmointi\\SuosikkiNimi\\pojat.txt");
+            string[] tytot = File.ReadAllLines("C:\\Users\\jyrili\\source\\T1-tasonohjelmointi\\SuosikkiNimi\\tytot.txt");
+            string nimi = NimiTB.Text;
+            foreach(string poju in pojat)
+            {
+                if(nimi == poju)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurip + ". suosituin poikien nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurip++;
+            }
+            foreach (string friidu in pojat)
+            {
+                if (nimi == friidu)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurit + ". suosituin tyttöjen nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurit++;
+            }
+            if(VastausLB.Visible == false)
+            {
+                VastausLB.Text = "Nimeäsi ei löytynyt suosituimpien nimien joukosta";
+                VastausLB.Visible = true;
+            }
+
+        }
+    }
+}
