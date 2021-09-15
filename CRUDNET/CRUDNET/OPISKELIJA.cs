0a7d042109ad92a3d616b43099319593352c608c
Jyri Lindroos <jyri.lindroos@keuda.fi>
2021-09-15 20:08:11 +0300

    oppilashallinta

---
+++ CRUDNET/CRUDNET/OPISKELIJA.cs
@@ -0,0 +1,135 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDNET
+{
+    /*
+     * Luodaan opiskelijaluokka seuraaville:
+     * - Uuden opiskelijan lisäämiseen
+     * - Olemassa olevan opiskelijan muokkaamiseen
+     * - Olemassa olevan opiskelijan poistamiseen
+     * - Kaikkien opiskelijoiden hakuun
+     */
+    class OPISKELIJA
+    {
+        YHDISTA yhteys = new YHDISTA();
+        // Luodaan funktio, jonka avulla lisätään uusi opiskelija
+
+        /*public DataTable asiakaslista()
+        {
+            MySqlCommand komento = new MySqlCommand("SELECT AsiakasId, Concat(Etunimi, ' ',Sukunimi) AS Kokonimi FROM asiakkaat", yhteys.otaYhteys());
+            MySqlDataAdapter adapteri = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+            // Lisätään Kokonimi-kenttä vastaukseen
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+            return taulu;
+        }*/
+        public bool lisaaOpiskelija(String enimi, String snimi, String puh, String email, int onro)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String lisayskysely = "INSERT INTO yhteystiedot " +
+                "(etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero) " +
+                "VALUES (@enm, @snm, @puh, @eml, @ono); ";
+            komento.CommandText = lisayskysely;
+            komento.Connection = yhteys.otaYhteys();
+            //@enm, @snm, @puh, @eml, @ono
+            komento.Parameters.Add("@enm", MySqlDbType.VarChar).Value = enimi;
+            komento.Parameters.Add("@snm", MySqlDbType.VarChar).Value = snimi;
+            komento.Parameters.Add("@puh", MySqlDbType.VarChar).Value = puh;
+            komento.Parameters.Add("@eml", MySqlDbType.VarChar).Value = email;
+            komento.Parameters.Add("@ono", MySqlDbType.UInt32).Value = onro;
+            
+
+
+            yhteys.avaaYhteys();
+            if (komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+
+
+        // Luodaan funktio kaikkien asiakastietojan hakemiseksi
+        public DataTable haeOpiskelijat()
+        {
+            MySqlCommand komento = new MySqlCommand("SELECT oid, etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero FROM yhteystiedot", yhteys.otaYhteys());
+            MySqlDataAdapter adapteri = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+
+            return taulu;
+        }
+
+        // Luodaan funktio asiakkaan tietojen muokkaamiseksi
+        public bool muokkaaOpiskelijaa(int oid, String enimi, String snimi, String puh, String email, int onro)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String paivityskysely = "UPDATE `yhteystiedot` SET `etunimi`= @enm," +
+                "`sukunimi`= @snm,`puhelin`= @puh,`sahkoposti`= @eml,`opiskelijanumero`= @ono" +
+                " WHERE oid = @oid";
+            komento.CommandText = paivityskysely;
+            komento.Connection = yhteys.otaYhteys();
+            //@enm, @snm, @puh, @eml, @ono
+            komento.Parameters.Add("@enm", MySqlDbType.VarChar).Value = enimi;
+            komento.Parameters.Add("@snm", MySqlDbType.VarChar).Value = snimi;
+            komento.Parameters.Add("@puh", MySqlDbType.VarChar).Value = puh;
+            komento.Parameters.Add("@eml", MySqlDbType.VarChar).Value = email;
+            komento.Parameters.Add("@ono", MySqlDbType.UInt32).Value = onro;
+            komento.Parameters.Add("@oid", MySqlDbType.UInt32).Value = oid;
+
+
+
+
+            yhteys.avaaYhteys();
+            if (komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+
+        // Luodaan funktio asiakkaan tietojen poistamiseen
+        // tähän tarvitaan vain käyttäjätunnus
+        public bool poistaOpiskelija(int ktunnus)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String poistokysely = "DELETE FROM asiakkaat WHERE oid = @oid";
+            komento.CommandText = poistokysely;
+            komento.Connection = yhteys.otaYhteys();
+            //@oid
+            komento.Parameters.Add("@oid", MySqlDbType.UInt32).Value = ktunnus;
+
+            yhteys.avaaYhteys();
+            if (komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+    }
+}
