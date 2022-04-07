7512b5f7c83bc783a3196b03ec1de524d18e7797
Jyri Lindroos <jyri.lindroos@keuda.fi>
2022-04-07 07:11:36 +0000

    CRUDNET 6.4.2022

--- CRUDNET/CRUDNET/OPISKELIJA.cs
+++ CRUDNET/CRUDNET/OPISKELIJA.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Windows.Forms;
 
 namespace CRUDNET
 {
@@ -20,7 +23,7 @@ namespace CRUDNET
         YHDISTA yhteys = new YHDISTA();
         // Luodaan funktio, jonka avulla lisätään uusi opiskelija
         
-        public DataTable asiakaslista()
+        /*public DataTable asiakaslista()
         {
             MySqlCommand komento = new MySqlCommand("SELECT oid, Concat(etunimi, ' ',sukunimi) AS Kokonimi FROM yhteystiedot", yhteys.otaYhteys());
             MySqlDataAdapter adapteri = new MySqlDataAdapter();
@@ -30,13 +33,15 @@ namespace CRUDNET
             adapteri.SelectCommand = komento;
             adapteri.Fill(taulu);
             return taulu;
-        }
+        }*/
         public bool lisaaOpiskelija(String enimi, String snimi, String puh, String email, int onro)
         {
+            String ktunnus = enimi.Substring(0, 3).ToLower() + snimi.Substring(0, 5).ToLower();
+            String salis = salasana();
             MySqlCommand komento = new MySqlCommand();
             String lisayskysely = "INSERT INTO yhteystiedot " +
-                "(etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero) " +
-                "VALUES (@enm, @snm, @puh, @eml, @ono); ";
+                "(etunimi, sukunimi, puhelin, sahkoposti, opiskelijanumero, ktunnus, salasana) " +
+                "VALUES (@enm, @snm, @puh, @eml, @ono, @usr, @ssa); ";
             komento.CommandText = lisayskysely;
             komento.Connection = yhteys.otaYhteys();
             //@enm, @snm, @puh, @eml, @ono
@@ -45,8 +50,9 @@ namespace CRUDNET
             komento.Parameters.Add("@puh", MySqlDbType.VarChar).Value = puh;
             komento.Parameters.Add("@eml", MySqlDbType.VarChar).Value = email;
             komento.Parameters.Add("@ono", MySqlDbType.UInt32).Value = onro;
-            
-
+            komento.Parameters.Add("@usr", MySqlDbType.VarChar).Value = ktunnus;
+            komento.Parameters.Add("@ssa", MySqlDbType.VarChar).Value = salis;
+            MessageBox.Show("Käyttäjätunnuksesi on " + ktunnus + "\nSalasanasi on " + salis + "\nkirjoita nämä visusti talteen");
 
             yhteys.avaaYhteys();
             if (komento.ExecuteNonQuery() == 1)
@@ -91,8 +97,7 @@ namespace CRUDNET
             komento.Parameters.Add("@eml", MySqlDbType.VarChar).Value = email;
             komento.Parameters.Add("@ono", MySqlDbType.UInt32).Value = onro;
             komento.Parameters.Add("@oid", MySqlDbType.UInt32).Value = oid;
-
-
+           
 
 
             yhteys.avaaYhteys();
@@ -113,7 +118,7 @@ namespace CRUDNET
         public bool poistaOpiskelija(int ktunnus)
         {
             MySqlCommand komento = new MySqlCommand();
-            String poistokysely = "DELETE FROM asiakkaat WHERE oid = @oid";
+            String poistokysely = "DELETE FROM yhteystiedot WHERE oid = @oid";
             komento.CommandText = poistokysely;
             komento.Connection = yhteys.otaYhteys();
             //@oid
@@ -131,5 +136,112 @@ namespace CRUDNET
                 return false;
             }
         }
+        public String salasana()
+        {
+            /*string EncryptedString = EncryptText(stringtoEncrypt, "Password");
+
+             string DecryptedString = DecryptText(EncryptedString, "Password"); */
+            char[] merkkijono = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            String ssana = "";
+            Random r =  new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                int rInt = r.Next(0, 61);
+                ssana += merkkijono[rInt];
+            }
+            return ssana;
+        }   
+
+        private string EncryptText(string Password)
+        {
+            // We are now going to create an instance of the Rihndael class. 
+            RijndaelManaged RijndaelCipher = new RijndaelManaged();
+
+            // First we need to turn the input strings into a byte array.
+            byte[] PlainText = System.Text.Encoding.Unicode.GetBytes(Password);
+
+            // We are using salt to make it harder to guess our key
+            // using a dictionary attack.
+            byte[] Salt = Encoding.ASCII.GetBytes(Password.Length.ToString());
+
+            // The (Secret Key) will be generated from the specified password and salt.
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Password, Salt);
+
+            // Create a encryptor from the existing SecretKey bytes.
+            // We use 32 bytes for the secret key 
+            // (the default Rijndael key length is 256 bit = 32 bytes) and
+            // then 16 bytes for the IV (initialization vector),
+            // (the default Rijndael IV length is 128 bit = 16 bytes)
+            ICryptoTransform Encryptor = RijndaelCipher.CreateEncryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+
+            // Create a MemoryStream that is going to hold the encrypted bytes 
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Create a CryptoStream through which we are going to be processing our data. 
+            // CryptoStreamMode.Write means that we are going to be writing data
+            // to the stream and the output will be written in the MemoryStream
+            // we have provided. (always use write mode for encryption)
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, Encryptor, CryptoStreamMode.Write);
+
+            // Start the encryption process.
+            cryptoStream.Write(PlainText, 0, PlainText.Length);
+
+            // Finish encrypting.
+            cryptoStream.FlushFinalBlock();
+
+            // Convert our encrypted data from a memoryStream into a byte array.
+            byte[] CipherBytes = memoryStream.ToArray();
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert encrypted data into a base64-encoded string.
+            // A common mistake would be to use an Encoding class for that.
+            // It does not work, because not all byte values can be
+            // represented by characters. We are going to be using Base64 encoding
+            // That is designed exactly for what we are trying to do. 
+
+            string EncryptedData = Convert.ToBase64String(CipherBytes);
+
+            // Return encrypted string.
+            return EncryptedData;
+        }
+        private static string DecryptText(string InputText, string Password)
+        {
+
+            RijndaelManaged RijndaelCipher = new RijndaelManaged();
+            byte[] EncryptedData = Convert.FromBase64String(InputText);
+            byte[] Salt = Encoding.ASCII.GetBytes(Password.Length.ToString());
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Password, Salt);
+
+            // Create a decryptor from the existing SecretKey bytes.
+            ICryptoTransform Decryptor = RijndaelCipher.CreateDecryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+
+            MemoryStream memoryStream = new MemoryStream(EncryptedData);
+
+            // Create a CryptoStream. (always use Read mode for decryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, Decryptor, CryptoStreamMode.Read);
+
+            // Since at this point we don't know what the size of decrypted data
+            // will be, allocate the buffer long enough to hold EncryptedData;
+            // DecryptedData is never longer than EncryptedData.
+            byte[] PlainText = new byte[EncryptedData.Length];
+
+            // Start decrypting.
+            int DecryptedCount = cryptoStream.Read(PlainText, 0, PlainText.Length);
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert decrypted data into a string.
+            string DecryptedData = Encoding.Unicode.GetString(PlainText, 0, DecryptedCount);
+
+            // Return decrypted string. 
+            return DecryptedData;
+        }
+
+
+
     }
 }
